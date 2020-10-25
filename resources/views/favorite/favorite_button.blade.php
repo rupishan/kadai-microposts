982efb6447c031d23616387ef000dc124d62478d
runa <rupi19.tech@gmail.com>
2020-10-25 07:53:00 +0000

    change favorite_button

--- resources/views/favorite/favorite_button.blade.php
+++ resources/views/favorite/favorite_button.blade.php
@@ -1,15 +1,13 @@
-@if(Auth::id() != $micropost->id)
-    @if(Auth::user()->is_favorite($micropost->id))
-        {{-- お気に入りから外すボタンのフォーム --}}
-        {!! Form::open(['route' => ['favorites.unfavorite',$micropost->id],'method'=>'delete']) !!}
-            {!! Form::submit('Unfavorite', ['class'=>"btn btn-success btn-sm"]) !!}
-        {!! Form::close() !!}
-    @else
-        {{-- お気に入りボタンのフォーム --}}
-        {!! Form::open(['route' => ['favorites.favorite',$micropost->id]]) !!}
-            {!! Form::submit('Favorite',['class' => "btn btn-light btn-sm"]) !!}
-        {!! Form::close() !!}
-    @endif
+@if(Auth::user()->is_favorite($micropost->id))
+    {{-- お気に入りから外すボタンのフォーム --}}
+    {!! Form::open(['route' => ['favorites.unfavorite',$micropost->id],'method'=>'delete']) !!}
+        {!! Form::submit('Unfavorite', ['class'=>"btn btn-success btn-sm"]) !!}
+    {!! Form::close() !!}
+@else
+    {{-- お気に入りボタンのフォーム --}}
+    {!! Form::open(['route' => ['favorites.favorite',$micropost->id]]) !!}
+        {!! Form::submit('Favorite',['class' => "btn btn-light btn-sm"]) !!}
+    {!! Form::close() !!}
 @endif
 
 
